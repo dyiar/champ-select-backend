70f2ca9edd8beba3414986b1ead9f65bca645b42
dyiar <austin.hl.blake@gmail.com>
2020-01-24 15:20:43 -0700

    finished users routes and middleware w/ authentication

--- api/usersRoutes/users.js
+++ api/usersRoutes/users.js
@@ -1,10 +1,8 @@
-// const express = require('express');
+const express = require('express');
 const db = require('../../dbconfig');
 const axios = require('axios');
-// require('dotenv').config();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const { authenticate } = require("../../auth/authenticate")
 
 const router = express.Router();
 
@@ -15,24 +13,56 @@ const base_url = "https://na1.api.riotgames.com/lol/"
 
 function generateToken(user) {
     const payload = {username: user.username };
+    const secret = process.env.JWT_SECRET;
+    const options = {
+        expiresIn: "120m"
+    };
+    return jwt.sign(payload, secret, options)
+}
+
+function register(req, res) {
+    const userInfo = req.body;
+    const hash = bcrypt.hashSync(userInfo.pw, 14);
+    userInfo.pw = hash;
+
+    return userInfo;
+}
+
+function login(req, res) {
+    const creds = req.body;
+
+    db('users')
+        .where({ username: creds.username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(creds.pw, user.pw)) {
+                const token = generateToken(user);
+                res.status(200).json({ message: `Welcome ${user.username}`, token });
+            } else {
+                res.status(401).json({
+                    message: "Login failed. Please enter the correct username and password."
+                })
+            }
+        })
+        .catch(() => res.status(500).send("Cannot reach server"));
 }
 
 //endpoints
 
-router.post('/newaccount', async (req, res, next) => {
-    // const accountName = req.params.accountName;
+router.post('/register', async (req, res, next) => {
+    const accountName = req.body.accountName;
     // const username = req.params.username;
     // const pw = req.params.pw;
-
-    console.log(req.body)
-
+    
     try {
+        
         let response = await axios.get(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${accountName}?api_key=${api_key}`)
-        console.log(response.data.accountId)
         let summonerid = await response.data.accountId
 
+        req.body = register(req);
+
         db('users')
-        .insert({username: username, pw: pw, summonerid: summonerid})
+        .insert({username: req.body.username, pw: req.body.pw, summonerid: summonerid})
         .then(ids => {
             res.status(201).send(ids)
         })
@@ -41,6 +71,10 @@ router.post('/newaccount', async (req, res, next) => {
     } catch (error) {
         return next(error)
     }
+})
+
+router.post('/login', login, (req, res) => {
+
 })
 
 module.exports = router;
