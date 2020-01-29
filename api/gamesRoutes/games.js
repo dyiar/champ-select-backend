22157c563da07e9b7f8d9d7bdfff66362f124519
dyiar <austin.hl.blake@gmail.com>
2020-01-29 16:57:28 -0700

    made a lot of progress on getting individual games and saving results in db

--- api/gamesRoutes/games.js
+++ api/gamesRoutes/games.js
@@ -9,7 +9,6 @@ const api_key = process.env.API_KEY;
 //middleware
 
 async function getAllGames(summonerid, beginningIndex) {
-    
     try {
         let response = await axios.get(`https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/${summonerid}?queue=420&queue=%20440&beginIndex=${beginningIndex}&api_key=${api_key}`)
         let gamesData = await response.data;
@@ -26,24 +25,28 @@ async function getSingleGames(gamesData, summonerid) {
     let startIndex = gamesData.startIndex
     let totalGames = gamesData.totalGames
     let endIndex = gamesData.endIndex
-
-    while (startIndex < endIndex) {
-        let response = await axios.get(`https://na1.api.riotgames.com/lol/match/v4/matches/${matches[startIndex].gameId}?api_key=${api_key}`)
-        let r1 = await response.participants[0].championId
-        let r2 = await response.participants[1].championId
-        let r3 = await response.participants[2].championId
-        let r4 = await response.participants[3].championId
-        let r5 = await response.participants[4].championId
-        let b1 = await response.participants[5].championId
-        let b2 = await response.participants[6].championId
-        let b3 = await response.participants[7].championId
-        let b4 = await response.participants[8].championId
-        let b5 = await response.participants[9].championId
+    let counter = 0
+    try {
+    while (startIndex <= endIndex) {
+        console.log(counter)
+        let response = await axios.get(`https://na1.api.riotgames.com/lol/match/v4/matches/${matches[counter].gameId}?api_key=${api_key}`)
+        console.log(response.data)
+        let r1 = await response.data.participants[0].championId
+        let r2 = await response.data.participants[1].championId
+        let r3 = await response.data.participants[2].championId
+        let r4 = await response.data.participants[3].championId
+        let r5 = await response.data.participants[4].championId
+        let b1 = await response.data.participants[5].championId
+        let b2 = await response.data.participants[6].championId
+        let b3 = await response.data.participants[7].championId
+        let b4 = await response.data.participants[8].championId
+        let b5 = await response.data.participants[9].championId
         
         let redBlue;
-        let result = true;
-        for (let i = 0; i<response.participantIdentities.length; i++) {
-            if (response.participantIdentities[i].accountId == summonerid) {
+        let result = 1;
+        
+        for (let i = 0; i<response.data.participantIdentities.length; i++) {
+            if (response.data.participantIdentities[i].player.accountId == summonerid) {
                 if (i < 5) {
                     redBlue = true
                 } else {
@@ -51,18 +54,44 @@ async function getSingleGames(gamesData, summonerid) {
                 }
             }
         }
-
-        if (redBlue == true && response.teams[0].win == true) {
-            result = true
-        } else if (redBlue === true && response.teams[0].win == false) {
-            result = false
+        
+        if (redBlue == false && response.data.teams[0].win == 'Win') {
+            result = 0
+        } else if (redBlue === true && response.data.teams[0].win == 'Fail') {
+            result = 0
         }
-
+            // console.log(response.data.gameId, r1, r2, r3, r4, r5, b1, b2, b3, b4, b5, result)
+            if(db('games').where({gameId: response.data.gameId}).first() == response.data.gameId) {
+                console.log('already inserted ' + startIndex)
+            } else {
             db('games')
-                .insert({gameId: response.gameId, r1: r1, r2: r2, r3: r3, r4: r4, r5: r5, b1: b1, b2: b2, b3: b3, b4: b4, b5: b5, result: result})
+                .insert({gameId: response.data.gameId, r1: r1, r2: r2, r3: r3, r4: r4, r5: r5, b1: b1, b2: b2, b3: b3, b4: b4, b5: b5, result: result}).then(() => {
+                }).catch((error) => console.log(error) )
+            }
 
-        startIndex += 1
+        if (counter == 98) {
+            break
+        }else {
+            counter += 1
+            startIndex += 1
+        }
+     
     }
+    if (endIndex < totalGames) {
+        console.log('about to call more games in 2 min')
+        setTimeout(() => {
+            console.log('calling more games')
+            getAllGames(summonerid, startIndex);
+        }, 123000);
+    } else {
+        console.log('exit single games')
+        return
+    }
+}
+catch (error){
+    return error
+}
+
 }
 
 //endpoints
