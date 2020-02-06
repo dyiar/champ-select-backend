e615278d93592d13bebb3f26de6c412e5dfb0998
dyiar <austin.hl.blake@gmail.com>
2020-02-06 15:49:23 -0700

    made changes so the knexconfig would go to pg if in production env

--- dbconfig.js
+++ dbconfig.js
@@ -1,4 +1,6 @@
 const knex = require('knex');
 const knexConfig = require('./knexfile');
+let environment = process.env.DB_ENV
+let client = knexConfig[environment]
 
-module.exports = knex(knexConfig.development);
+module.exports = knex(client);
